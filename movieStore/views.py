2e83cd00652ecd1a9f2379a3a526afebe58e2c0e
antony-g-c <111447984+antony-g-c@users.noreply.github.com>
2025-02-05 14:04:56 -0500

    Update views.py

--- movieStore/views.py
+++ movieStore/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse  
+from django.contrib.auth import login, authenticate  
+from core.forms import UserCreationForm
+
 
 # Create your views here.
+def home(request):
+    count= User.objects.count()
+    return render(request, 'home.html',{'count':count})
+
+
+def signup(request):
+    if request.method == 'POST':  
+        form = UserCreationForm(request.POST)  
+        if form.is_valid():  
+            form.save()  
+            return redirect('home')
+    else:  
+        form = UserCreationForm()    
+    return render(request, 'registration/signup.html', {'form':form})
