42b16dc6d9788d7b18503b4d4544fa7e2c8167f2
Arthur Wang <awang728@gatech.edu>
2025-02-06 09:15:50 -0500

    allow users to see and search movies

--- movieStore/models.py
+++ movieStore/models.py
@@ -1,3 +1,20 @@
+import datetime
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=100)
+    release_date = models.DateField('release date')
+    def __str__(self):
+        return self.movie_name
+
+    @admin.display(
+        boolean=True,
+        ordering='release_date',
+        description="Recent release?"
+    )
+    def was_released_recently(self):
+        now = timezone.now().date()
+        return now - datetime.timedelta(days=365) <= self.release_date
--- movieStore/views.py
+++ movieStore/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Movie
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "movieStore/index.html"
+    context_object_name = "movie_list"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Movie.objects.filter(movie_name__icontains=query)
+        return Movie.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Movie
+    template_name = "movieStore/detail.html"
