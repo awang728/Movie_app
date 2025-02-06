f6d3c0176af67dc33add098ea1e4a05f393b5b53
Hassan Asani <hassanasani2004@gmail.com>
2025-02-06 00:29:15 -0500

    updated correctly this time lol

--- movieStore/views.py
+++ movieStore/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie, Review
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    template_data = {}
+    template_data['title'] = 'Movies'
+    template_data['movies'] = movies
+    return render(request, 'movies/index.html',
+                  {'template_data': template_data})
+
+# Code for each individual movie
+def show(request, id):
+    movie = Movie.objects.get(id=id)
+    template_data = {}
+    template_data['title'] = movie.name
+    template_data['movie'] = movie
+    return render(request, 'movies/show.html',
+                  {'template_data': template_data})
+
+
+
