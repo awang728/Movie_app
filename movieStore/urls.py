6f7a3b4d955b3b762b3c54557c47bb677baabe48
Tisha1503 <130492564+Tisha1503@users.noreply.github.com>
2025-02-06 21:13:08 -0500

    Update urls.py

--- movieStore/urls.py
+++ movieStore/urls.py
@@ -6,4 +6,7 @@ app_name = "movieStore"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path('signup/', signup, name='signup'),
+    path('add-to-cart/<int:movie_id>/', add_movie_to_cart, name='add_to_cart'),
+    path('cart/', cart, name='cart'),
 ]
