9a2f881edaa42362d98b11156f3c742e25ed8004
antony-g-c <antony.g.chackieth@gmail.com>
2025-02-07 16:33:05 -0500

    fixed some errors with login redirects

--- movieStore/views.py
+++ movieStore/views.py
@@ -19,18 +19,6 @@ def home(request):
     count= User.objects.count()
     return render(request, 'home.html',{'count':count})
 
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form':form})
-
-
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = "movieStore/index.html"
