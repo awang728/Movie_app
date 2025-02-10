843b9406bf13d78613ad3d40cd3457726c9582b9
antony-g-c <111447984+antony-g-c@users.noreply.github.com>
2025-02-10 16:25:27 -0500

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse  
+from django.contrib.auth import login, authenticate  
+from core.forms import UserCreationForm
+"""
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_text  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .tokens import account_activation_token   
+from django.core.mail import EmailMessage
+"""
+
+# Create your views here.
+def home(request):
+    count= User.objects.count()
+    return render(request, 'home.html',{'count':count})
+
+def signup(request):
+    if request.method == 'POST':  
+        form = UserCreationForm(request.POST)  
+        if form.is_valid():  
+            form.save()  
+            return redirect('home')
+    else:  
+        form = UserCreationForm()    
+    return render(request, 'registration/signup.html', {'form':form})
