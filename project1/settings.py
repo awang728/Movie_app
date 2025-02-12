260dd509e4f4ce28f5b607626306a50e9d7a7b64
antony-g-c <antony.g.chackieth@gmail.com>
2025-02-12 18:23:03 -0500

    Major GUI changes

--- project1/settings.py
+++ project1/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'movieStore/static/'
+STATICFILES_DIRS = [BASE_DIR / "movieStore/static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
