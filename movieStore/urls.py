d09871dbf98c69fa67cc8d204a51417daf606ae2
hassan asani <149350085+hassanasani@users.noreply.github.com>
2025-02-13 14:59:57 -0500

    Update urls.py

--- movieStore/urls.py
+++ movieStore/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('movies', views.IndexView.as_view(), name="movies"),
+    path("<int:id>/review/create/", views.create_review, name="create_review"),
+    path("<int:id>/review/<int:review_id>/edit/", views.edit_review, name="edit_review"),
+    path("<int:id>/review/<int:review_id>/delete/", views.delete_review, name="delete_review"),
 ]
