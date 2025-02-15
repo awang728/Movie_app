385c45436db757f114220196e33069b3719b2134
antony-g-c <111447984+antony-g-c@users.noreply.github.com>
2025-02-14 19:14:50 -0500

    fix

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "cart"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('<int:id>/add/', views.add_to_cart, name='add_to_cart'),
     path('clear/', views.clear, name='clear'),
     path('add_multiple_items_of_movie_to_cart/<int:movie_id>/', views.add_multiple_items_of_movie_to_cart, name='add_multiple_items_of_movie_to_cart'),
 
