904014e2ea1992823d161cebd30daae357126bcd
antony-g-c <111447984+antony-g-c@users.noreply.github.com>
2025-02-14 19:28:37 -0500

    Update urls.py orders page

--- cart/urls.py
+++ cart/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:id>/add/', views.add_to_cart, name='add_to_cart'),
     path('clear/', views.clear, name='clear'),
     path('purchase/', views.purchase, name='cart.purchase'),
+    path('orders/', views.orders, name='accounts.orders'),
     path('add_multiple_items_of_movie_to_cart/<int:movie_id>/', views.add_multiple_items_of_movie_to_cart, name='add_multiple_items_of_movie_to_cart'),
 
 ]
